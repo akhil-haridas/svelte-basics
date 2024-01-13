7849f462a19fde8851ea4e5ada6a7ecc78e67dfd
akhil-haridas <4khilharidas@gmail.com>
2024-01-13 14:04:30 +0530

    array element changed into object

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
-	const mobileCompanies = ["Apple","Pixel","Nothing","Samsung","OnePlus"]
+	// const mobileCompanies = ["Apple","Pixel","Nothing","Samsung","OnePlus"]
+	const mobileCompanies = [{name:"Apple"},{name:"Pixel"},{name:"Nothing"},{name:"Samsung"},{name:"OnePlus"}]
+
 </script>
 
 <div>
-	{#each mobileCompanies as comapny,index}
-		<h1>{index + 1}{comapny}</h1>
+	{#each mobileCompanies as company}
+		<h1>{index + 1}{company}</h1>
 	{/each}	
 </div>
