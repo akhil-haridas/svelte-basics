b3ecba65730c0429a5350ad145e59dd46b9fada1
akhil-haridas <4khilharidas@gmail.com>
2024-01-13 14:12:49 +0530

    shuffling array

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,23 @@
 <script>
-	// const mobileCompanies = ["Apple","Pixel","Nothing","Samsung","OnePlus"]
-	const mobileCompanies = [{name:"Apple"},{name:"Pixel"},{name:"Nothing"},{name:"Samsung"},{name:"OnePlus"}]
+  import { shuffle } from "lodash";
+  // const mobileCompanies = ["Apple","Pixel","Nothing","Samsung","OnePlus"]
+  let mobileCompanies = [
+    { name: "Apple" },
+    { name: "Pixel" },
+    { name: "Nothing" },
+    { name: "Samsung" },
+    { name: "OnePlus" },
+  ];
 
-	const shuffleArray = () => {
-		
-	}
+  const shuffleArray = () => {
+    mobileCompanies = shuffle(mobileCompanies);
+  };
 </script>
 
 <div>
-	{#each mobileCompanies as company}
-	<h1>{company.name}</h1>
-	<input type="text" placeholder="Enter name" />
-	{/each}
-	<button type="button" on:click={shuffleArray}>SHUFFLE</button>	
+  {#each mobileCompanies as company}
+    <h1>{company.name}</h1>
+    <input type="text" placeholder="Enter name" />
+  {/each}
+  <button type="button" on:click={shuffleArray}>SHUFFLE</button>
 </div>
