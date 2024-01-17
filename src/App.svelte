0b051e912c5fdd73e31289c62f176eba0951ebb8
akhil-haridas <4khilharidas@gmail.com>
2024-01-17 19:58:39 +0530

    using doubled variable

--- src/App.svelte
+++ src/App.svelte
@@ -38,6 +38,7 @@
 <div>
   <h1>Event Handling in svelte</h1>
   <h2>Count : {count}</h2>
+  <h2>Doubled : {doubled}</h2>
   <button on:click={increment}>+</button>
   <button on:click={addon}>Addon</button>
 </div> 
