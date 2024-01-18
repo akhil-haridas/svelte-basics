4158c66d20ba59a6dc24c5408d0099ffc8acfc17
akhil-haridas <4khilharidas@gmail.com>
2024-01-18 20:47:58 +0530

    function for increasing

--- src/App.svelte
+++ src/App.svelte
@@ -41,7 +41,7 @@ let volume =0;
 
 <div>
   <h1>VOLUME : {volume}</h1>
-  <button>Increase Button</button>
+  <button on:click={()=> volume = volume + 1}>Increase Button</button>
   <button>Decrease Button</button>
 </div>
 
