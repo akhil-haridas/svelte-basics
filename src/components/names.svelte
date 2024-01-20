4e58b429e32f3def2ae747ff95817b6c06fb2971
akhil-haridas <4khilharidas@gmail.com>
2024-01-20 20:09:43 +0530

    created a array

---
+++ src/components/names.svelte
@@ -0,0 +1,12 @@
+<script>
+    const nameList = [
+        {
+            firstName :"Akhil",
+            lastName : "k"
+        },
+        {
+            firstName : "Sagar",
+            lastName : "Samanta"
+        }
+    ]
+</script>
