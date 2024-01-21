927bd775485157f2a1eb4cd39d67a46ac7850f78
akhil-haridas <4khilharidas@gmail.com>
2024-01-21 18:21:50 +0530

    styling

--- src/components/fetchtodo.svelte
+++ src/components/fetchtodo.svelte
@@ -2,6 +2,27 @@
 
 </script>
 
-<main>
+<main class="todo-list-wrapper">
     <h1>TODO LIST </h1>
+    <div class="todo-wrapper">
+        <span>✅</span>
+        <h3>Todo item</h3>
+    </div>
 </main>
+
+<style>
+    .todo-list-wrapper {
+        background-color: black;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+    }
+
+    .todo-wrapper {
+        display: flex;
+        place-items: center;
+        width: 100%;
+        background-color: darkgray;
+    }
+</style>
