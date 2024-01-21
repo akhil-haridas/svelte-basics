0a2a53492f1f20c7c029ad37a382a355c0890d9f
akhil-haridas <4khilharidas@gmail.com>
2024-01-21 17:55:37 +0530

    imported

--- src/components/mount.svelte
+++ src/components/mount.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { onMount, onDestroy  } from 'svelte'
+    import { onMount, onDestroy, beforeUpdate, afterUpdate  } from 'svelte'
 
     // onMount(()=>{
     //     console.log("Mounting....")
